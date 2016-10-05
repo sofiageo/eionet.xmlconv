a3a0e59b5a37914debc273919d4eeaab68a8eeee
vasilis skiadas <vs@eworx.gr>
2016-10-05 19:11:58 +0300

    QaController Instant execution of Job api endpoint

--- src/main/java/eionet/gdem/api/qa/web/QaController.java
+++ src/main/java/eionet/gdem/api/qa/web/QaController.java
@@ -9,6 +9,7 @@ import eionet.gdem.qa.XQueryService;
 import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import java.util.Hashtable;
+import java.util.LinkedHashMap;
 import java.util.Vector;
 import javax.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
@@ -42,39 +43,39 @@ public class QaController {
 
     /**
      * Perform a Qa Job instantly
-     **/
-    @RequestMapping(value = "/sendQARequest", method = RequestMethod.POST,consumes = "application/json", produces = "application/json")
-    public ResponseEntity<HashMap<String,String>> sendQARequest(@RequestBody EnvelopeWrapper envelopeWrapper) throws QaServiceException, EmptyParameterException {
+     *
+     */
+    @RequestMapping(value = "/qajobs", method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
+    public ResponseEntity<HashMap<String, String>> sendQARequest(@RequestBody EnvelopeWrapper envelopeWrapper) throws QaServiceException, EmptyParameterException ,UnsupportedEncodingException {
 
-        if (envelopeWrapper.getSourceUrl()==null) {
+        if (envelopeWrapper.getSourceUrl() == null) {
             throw new EmptyParameterException("source_url");
         }
-        if (envelopeWrapper.getScriptId()==null) {
+        if (envelopeWrapper.getScriptId() == null) {
             throw new EmptyParameterException("script_id");
         }
-        
-       Vector results =  qaService.runQaScript(envelopeWrapper.getSourceUrl(), envelopeWrapper.getScriptId());
-     
-       HashMap<String,String> jsonResults = new HashMap<String,String>();
-       jsonResults.put("contentType", results.get(0).toString());
-       
-       byte[] bytes = (byte[])results.get(1);
-        try {
-            String str = new String(bytes, "UTF-8");
-            jsonResults.put("results",str);
-        } catch (UnsupportedEncodingException ex) {
-            jsonResults.put("results",results.get(1).toString());
-        }
-       return new ResponseEntity<HashMap<String,String>>(jsonResults, HttpStatus.OK);
+
+        Vector results = qaService.runQaScript(envelopeWrapper.getSourceUrl(), envelopeWrapper.getScriptId());
+
+        LinkedHashMap<String, String> jsonResults = new LinkedHashMap<String, String>();
+
+        jsonResults.put("feedbackStatus",ByteArrayToString((byte[]) results.get(2)));
+        jsonResults.put("feedbackMessage", ByteArrayToString((byte[]) results.get(3)));
+        jsonResults.put("feedbackContentType", results.get(0).toString());
+        jsonResults.put("feedbackContent", ByteArrayToString((byte[]) results.get(1)));
+
+
+        return new ResponseEntity<HashMap<String, String>>(jsonResults, HttpStatus.OK);
     }
 
-   /**
+    /**
      * Schedule a Qa Job
-     **/
-    @RequestMapping(value = "/analyzeEnvelope", method = RequestMethod.POST,consumes = "application/json", produces = "application/json")
-    public ResponseEntity<Vector> scheduleQaRequest(@RequestBody  EnvelopeWrapper envelopeWrapper) throws QaServiceException ,EmptyParameterException {
+     *
+     */
+    @RequestMapping(value = "/analyzeEnvelope", method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
+    public ResponseEntity<Vector> scheduleQaRequest(@RequestBody EnvelopeWrapper envelopeWrapper) throws QaServiceException, EmptyParameterException  {
 
-        if (envelopeWrapper.getEnvelopeUrl()==null) {
+        if (envelopeWrapper.getEnvelopeUrl() == null) {
             throw new EmptyParameterException("envelope_url");
         }
         HashMap<String, String> fileSchemasAndLinks = qaService.extractSchemasAndFilesFromEnvelopeUrl(envelopeWrapper.getEnvelopeUrl());
@@ -84,14 +85,14 @@ public class QaController {
 
     }
 
-    
     /**
      * Authorized Schedule a Qa Job
-     **/
-    @RequestMapping(value = "/auth/analyzeEnvelope", method = RequestMethod.POST,consumes = "application/json", produces = "application/json")
-    public ResponseEntity<Vector> SecuredScheduleQaRequest(@RequestBody  EnvelopeWrapper envelopeWrapper) throws QaServiceException ,EmptyParameterException {
+     *
+     */
+    @RequestMapping(value = "/auth/analyzeEnvelope", method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
+    public ResponseEntity<Vector> SecuredScheduleQaRequest(@RequestBody EnvelopeWrapper envelopeWrapper) throws QaServiceException, EmptyParameterException {
 
-        if (envelopeWrapper.getEnvelopeUrl()==null) {
+        if (envelopeWrapper.getEnvelopeUrl() == null) {
             throw new EmptyParameterException("envelope_url");
         }
         HashMap<String, String> fileSchemasAndLinks = qaService.extractSchemasAndFilesFromEnvelopeUrl(envelopeWrapper.getEnvelopeUrl());
@@ -100,54 +101,55 @@ public class QaController {
         return new ResponseEntity<Vector>(jobIds, HttpStatus.OK);
 
     }
-    
+
     @RequestMapping(value = "/getQAResults/{jobId}", method = RequestMethod.GET)
     public ResponseEntity<Hashtable> getQAResultsForJob(@PathVariable String jobId) throws XMLConvException {
 
         XQueryService xqueryService = new XQueryService();
         Hashtable results = xqueryService.getResult(jobId);
-    
+
         return new ResponseEntity<Hashtable>(results, HttpStatus.OK);
     }
 
-    
-    @RequestMapping(value="/listQueries",method = RequestMethod.GET)
-    public ResponseEntity<Vector> listQeuries(@RequestParam String schema)throws XMLConvException{
-    
+    @RequestMapping(value = "/listQueries", method = RequestMethod.GET)
+    public ResponseEntity<Vector> listQeuries(@RequestParam String schema) throws XMLConvException {
+
         XQueryService xqueryService = new XQueryService();
         Vector results = xqueryService.listQueries(schema);
-        
-        return new ResponseEntity<Vector>(results,HttpStatus.OK);
-    }
-    
-  /*  
-    @ExceptionHandler(JobNotFoundException.class)
-    public void handleJobNotFoundException(JobNotFoundException exception, HttpServletResponse response) {
-        exception.printStackTrace();
-        response.setStatus(HttpServletResponse.SC_NOT_FOUND);
-   
+
+        return new ResponseEntity<Vector>(results, HttpStatus.OK);
     }
 
-   */ 
+
+     
     @ExceptionHandler(QaServiceException.class)
     public void HandleQaServiceException(Exception exception, HttpServletResponse response) {
         exception.printStackTrace();
         response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
     }
-    
+
     /**
-     *This Exception is quite generic and common so we should consider moving it to a Global Exception Handler class with @ControllerAdvice 
-     **/
+     * This Exception is quite generic and common so we should consider moving
+     * it to a Global Exception Handler class with @ControllerAdvice 
+     *
+     */
     @ExceptionHandler(EmptyParameterException.class)
-    public ResponseEntity<HashMap<String,String>> HandleEmptyParameterException(Exception exception) {
+    public ResponseEntity<HashMap<String, String>> HandleEmptyParameterException(Exception exception) {
         exception.printStackTrace();
-        HashMap<String,String> errorResult = new HashMap<String,String>();
-        errorResult.put("error message",exception.getMessage());
-        return new ResponseEntity<HashMap<String,String>>(errorResult,HttpStatus.BAD_REQUEST);
+        HashMap<String, String> errorResult = new HashMap<String, String>();
+        errorResult.put("error message", exception.getMessage());
+        return new ResponseEntity<HashMap<String, String>>(errorResult, HttpStatus.BAD_REQUEST);
     }
+
     @ExceptionHandler(XMLConvException.class)
     public void HandleGDEMException(Exception exception, HttpServletResponse response) {
         exception.printStackTrace();
         response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+    }
+
+    
+    public String ByteArrayToString(byte[] bytes) throws UnsupportedEncodingException {
+
+        return new String(bytes, "UTF-8");
     }
 }
